8d3f895a09f26db2e557c43f295b2ee848c67d9f
sagekhanuja <sagek1@gmail.com>
2023-06-02 15:45:24 -0700

    docs

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+function displayTodoData() {
+  const endpoint = "https://jsonplaceholder.typicode.com/todos/1";
+  fetch(endpoint)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const table = document.getElementById("todo-table");
+      const row = table.insertRow();
+      const userIdCell = row.insertCell(0);
+      const idCell = row.insertCell(1);
+      const titleCell = row.insertCell(2);
+      const completedCell = row.insertCell(3);
+      userIdCell.innerHTML = data.userId;
+      idCell.innerHTML = data.id;
+      titleCell.innerHTML = data.title;
+      completedCell.innerHTML = data.completed;
+    });
+}
+
+window.addEventListener("load", function () {
+  displayTodoData();
+  console.log("window loaded");
+});
+
 document.getElementById("test-button").addEventListener("click", function () {
   console.log("button clicked");
 });
